43d21831f9d97970bbbdb79f126e2ec008fd1fe5
Ali Amin <aamindehkordi@gmail.com>
2023-07-08 20:54:33 -0700

    helper functions and clone impl

--- src/model/tile.rs
+++ src/model/tile.rs
@@ -1,14 +1,38 @@
 use crate::model::pieces::piece::{Color, Piece, PieceType};
 
-// An abstract Tile class that represents a tile on the board with proper notation.
 pub struct Tile {
-    pub position: (usize, usize),
-    pub piece: Option<Box<dyn Piece>>,
+    pub(crate) position: (usize, usize),
+    pub(crate) piece: Option<Box<dyn Piece>>,
+    pub(crate) attacked_by: Vec<(usize, usize)>,
+}
+
+impl PartialEq for Tile {
+    fn eq(&self, other: &Self) -> bool {
+        self.position == other.position && match (&self.piece, &other.piece) {
+            (Some(this_piece), Some(other_piece)) => {
+                this_piece.get_color() == other_piece.get_color()
+                && this_piece.get_type() == other_piece.get_type()
+                && this_piece.get_position() == other_piece.get_position()
+            }
+            (None, None) => true,
+            _ => false,
+        }
+    }
+}
+
+impl Clone for Tile {
+    fn clone(&self) -> Self {
+        Tile {
+            position: self.position,
+            piece: self.piece.as_ref().map(|piece| piece.clone_box()),
+            attacked_by: self.attacked_by.clone(),
+        }
+    }
 }
 
 impl Tile {
     pub fn new(position: (usize, usize), piece: Option<Box<dyn Piece>>) -> Self {
-        Self { position, piece }
+        Self { position, piece, attacked_by: Vec::new() }
     }
 
     // Getters
@@ -29,6 +53,10 @@ impl Tile {
 
     pub fn set_piece(&mut self, piece: Option<Box<dyn Piece>>) {
         self.piece = piece;
+    }
+
+    pub fn attacked(&mut self, pos: (usize, usize)) {
+        self.attacked_by.push(pos);
     }
 
 }
