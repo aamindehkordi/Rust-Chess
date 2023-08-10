e1103ecc615469152813bb46915f9eb204f6db00
Ali Amin <aamindehkordi@gmail.com>
2023-08-09 18:06:56 -0700

    rename/ refactor move

--- backend/src/moves.rs
+++ backend/src/moves.rs
@@ -2,32 +2,12 @@ use crate::board::*;
 use crate::piece::*;
 
 pub const DIRECTION_OFFSETS: [i8; 8] = [-9, -8, -7, -1, 1, 7, 8, 9];
-pub type Moves = Vec<Move>;
+pub type Moves = Vec<SimpleMove>;
 
-#[derive(Debug, Copy, Clone)]
 /// A move is a pair of positions.
 /// The first position is the position of the piece to move.
 /// The second position is the position to move the piece to.
-pub struct Move {
-    pub from: Position,
-    pub to: Position,
-}
-
-impl Move {
-    /// Creates a new move.
-    ///
-    /// # Arguments
-    /// * `from` - The position of the piece to move.
-    /// * `to` - The position to move the piece to.
-    ///
-    /// # Example
-    /// ```rs
-    ///     let move_ = Move::new(0, 1);
-    /// ```
-    pub fn new(from: Position, to: Position) -> Move {
-        Move { from, to }
-    }
-}
+pub type SimpleMove = (Position, Position);
 
 /// Generates all possible moves for a given board.
 ///
